e96b3ae2fd7c6512a3091221eb1b7b0eccbb17c1
byigitt <137426514+byigitt@users.noreply.github.com>
2025-01-13 19:20:07 +0300

    feat(init): create __init__.py for notebook translator package with versioning and exports

---
+++ src/notebook_translator/__init__.py
@@ -0,0 +1,8 @@
+"""Notebook Markdown Translator package."""
+
+__version__ = "0.1.0"
+
+from .markdown_extractor import MarkdownExtractor
+from .types import MarkdownContent
+
+__all__ = ["MarkdownExtractor", "MarkdownContent"] 
